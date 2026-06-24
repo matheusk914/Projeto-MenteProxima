2bdcec72a43cd835110b5a07a8f2b7edb3255328
matheusk914 <matheusk914@gmail.com>
2026-06-24 17:18:38 -0300

    Revert "Projeto MenteProxima - Remove botão Começar"
    This reverts commit 719bb542a2a881c91073e4c7ed75fa29a250d1aa.

--- src/componentes/BadgeGratuidade.jsx
+++
@@ -1,8 +0,0 @@
-function BadgeGratuidade({ gratuito }) {
-    if (gratuito) {
-        return <span className="badge-gratuito">Gratuito</span>;
-    }
-    return <span className="badge-pago">Pago</span>;
-}
-
-export default BadgeGratuidade;
